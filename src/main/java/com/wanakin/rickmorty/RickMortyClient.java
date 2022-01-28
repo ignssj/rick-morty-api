f077f1e075f588e68d8dbcdcaa752129a8dc3d3a
Ignacio P Garcia <52336602+ignssj@users.noreply.github.com>
2022-01-28 12:31:39 -0300

    Update RickMortyClient.java
    spring boot dev tools + error msg for bad parameters

--- src/main/java/com/wanakin/rickmorty/RickMortyClient.java
+++ src/main/java/com/wanakin/rickmorty/RickMortyClient.java
@@ -7,6 +7,7 @@ import com.wanakin.rickmorty.response.ListEpisodesResponse;
 import com.wanakin.rickmorty.response.ListLocationsResponse;
 import com.wanakin.rickmorty.response.LocationResponse;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -26,17 +27,17 @@ public class RickMortyClient {
 
     public Mono<CharacterResponse> findMyCharacter(String id){
     log.info("Searching for Character [{}]",id);
-    return webClient.get().uri("/character/"+ id).accept(MediaType.APPLICATION_JSON).retrieve().bodyToMono(CharacterResponse.class);
+    return webClient.get().uri("/character/"+ id).accept(MediaType.APPLICATION_JSON).retrieve().onStatus(HttpStatus::is4xxClientError, error -> Mono.error(new RuntimeException("Error in parameters"))).bodyToMono(CharacterResponse.class);
     }
 
     public Mono<LocationResponse> findMyLocation(String id){
         log.info("Searching for location of [{}]",id);
-        return webClient.get().uri("/location/"+id).accept(MediaType.APPLICATION_JSON).retrieve().bodyToMono(LocationResponse.class);
+        return webClient.get().uri("/location/"+id).accept(MediaType.APPLICATION_JSON).retrieve().onStatus(HttpStatus::is4xxClientError, error -> Mono.error(new RuntimeException("Error in parameters"))).bodyToMono(LocationResponse.class);
     }
 
     public Mono<EpisodeResponse> findMyEpisode(String id){
         log.info("Searching for episode [{}]",id);
-        return webClient.get().uri("/episode/"+id).accept(MediaType.APPLICATION_JSON).retrieve().bodyToMono(EpisodeResponse.class);
+        return webClient.get().uri("/episode/"+id).accept(MediaType.APPLICATION_JSON).retrieve().onStatus(HttpStatus::is4xxClientError, error -> Mono.error(new RuntimeException("Error in parameters"))).bodyToMono(EpisodeResponse.class);
     }
 
     public Flux<ListCharactersResponse> listCharacters(){
